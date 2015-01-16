e72d1b1e8fa645b2e9c0092197750b09080f4254
haroldo <haroldoramirez@gmail.com>
2015-01-16 16:51:28 -0200

    melhorado listas e implementado parcialmente o paginate no backend(play2)

--- app/controllers/PaisController.java
+++ app/controllers/PaisController.java
@@ -2,6 +2,8 @@ package controllers;
 
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.FetchConfig;
+import com.avaje.ebean.Page;
+import com.avaje.ebean.PagingList;
 import models.locale.Pais;
 import play.Logger;
 import play.libs.Json;
@@ -9,6 +11,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import javax.persistence.PersistenceException;
+import java.util.List;
 
 public class PaisController extends Controller {
 
@@ -56,13 +59,56 @@ public class PaisController extends Controller {
     }
 
 
-
     //Mostrar acima de 16 linhas
-    public static Result buscaPorPaginas(Integer paginas) {
-        Logger.info("busca por páginas");
+    public static Result buscaPorPaginas(Integer pagina) {
+        Logger.info("busca por página");
+
+        //--------Estudos sobre Paginate Ebean
+        //setFetchAhead -> pŕoxima página é carregada em segundo plano
+        //getFutureRowCount -> total de contagem de linhas
+        //getAsList() -> Retornar os dados para todas as páginas na forma de uma lista única.
+        //getPageSize -> Retorne o tamanho da página. Este é o número de linhas por página.
+        //getTotalRowCount -> Retorne a contagem total de linhas.
+        //getTotalPageCount -> Retorne a contagem total de páginas.
+
         //find.where().findPagingList(tamanhoDaPagina).setFetchAhead(true).getPage(numeroDaPagina)
-        //return ok(Json.toJson(Ebean.find(Pais.class).setMaxRows(16).findList()));
-       return ok(Json.toJson(Ebean.find(Pais.class).where().findPagingList(16).setFetchAhead(true).getPage(paginas)));
+        //return ok(Json.toJson(Ebean.find(Pais.class).setMaxRows(5).findList()));
+        //return ok(Json.toJson(Ebean.find(Pais.class).where().findPagingList(10).setFetchAhead(false).getPage(pagina)));
+        //------------------------------------------------------------------------------------------------------
+        //int tamanhoPagina = 10;
+
+//        PagingList<Pais> pagingList =
+//                Ebean.find(Pais.class)
+//                        .where().gt("nome", "2")
+//                        .findPagingList(10);
+//
+//        // get the row count in the background...
+//        // ... otherwise it is fetched on demand
+//        // ... when getRowCount() or getPageCount()
+//        // ... is called
+//        pagingList.getFutureRowCount();
+//
+//        // get the first page
+//        Page<Pais> page = pagingList.getPage(0);
+//
+//        // get the beans from the page as a list
+//        List<Pais> list = page.getList();
+//        return ok(Json.toJson(list));
+
+       PagingList<Pais> pagingList =
+                Ebean.find(Pais.class)
+                        .order()
+                        .asc("nome")
+                        .where().gt("nome", "2")
+                        .findPagingList(15).setFetchAhead(true);
+
+        pagingList.getFutureRowCount();
+
+        Page<Pais> page = pagingList.getPage(pagina);
+
+        List<Pais> list = page.getList();
+
+        return ok(Json.toJson(list));
     }
 
     public static Result remover(Integer id) {
