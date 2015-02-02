fcee8f3f8fd5db65df0a93997e1b0ee18e45e23b
haroldo <haroldoramirez@gmail.com>
2015-02-02 17:22:13 -0200

    melhoado carregamento e menu

--- public/javascripts/loadingScreen.js
+++ public/javascripts/loadingScreen.js
@@ -1,49 +1,65 @@
 
 function openLoadingScreen() {
-
-    var div = document.createElement("div");
-    window.__load.div = div;
-    div.style.position = "fixed";
-    div.style.top = "0px";
-    div.style.left = "0px";
-    div.style.zIndex = "2147483647";
-    div.style.right = "0px";
-    div.style.bottom = "0px";
-    div.style.backgroundColor = "rgba(14,53,69,0.5)";
     
-    var table = document.createElement("table"),
-    tr = document.createElement("tr"),
-    td = document.createElement("td");
+    if(window.__load.div == null){
+        
+        var div = document.createElement("div");
+        window.__load.div = div;
+        div.style.position = "fixed";
+        div.style.top = "0px";
+        div.style.left = "0px";
+        div.style.zIndex = "2147483647";
+        div.style.right = "0px";
+        div.style.bottom = "0px";
+        div.style.opacity = "0";
+        div.style.backgroundColor = "rgba(0,0,0,0.08)";
+        
+        var table = document.createElement("table"),
+        tr = document.createElement("tr"),
+        td = document.createElement("td");
+        
+        //td.innerHTML = "Carregando...";
+        //td.style.color = "#fff";
+        td.style.fontFamily = "Arial";
+        td.style.fontSize = "30px";
     
-    td.innerHTML = "Carregando...";
-    td.style.color = "#fff";
-    td.style.fontFamily = "Arial";
-    td.style.fontSize = "30px";
-
-    div.appendChild(table);
-    table.appendChild(tr);
-    tr.appendChild(td);
+        div.appendChild(table);
+        table.appendChild(tr);
+        tr.appendChild(td);
+        
+        table.style.height = "100%";
+        table.style.marginLeft = "auto";
+        table.style.marginRight = "auto";
+        
+        document.body.appendChild(div);
+        
+    }
     
-    table.style.height = "100%";
-    table.style.marginLeft = "auto";
-    table.style.marginRight = "auto";
+    $(div).animate({
+        opacity: 1
+    },window.__load.fadeDuration);
     
-    document.body.appendChild(div);
 }
 
 function closeLoadingScreen() {
-    window.__load.div.remove();
+    $(window.__load.div).animate({
+        opacity: 0
+    },window.__load.fadeDuration,function(){
+        $(this).remove();
+        window.__load.div = null;
+    });
 }
 
 window.__load = {
+    fadeDuration: 175,
     counter: 0,
     ini: function(){
         if(this.counter==0)openLoadingScreen();
         this.counter ++;
     },
     end: function(){
-        this.counter --;
-        if(this.counter==0)closeLoadingScreen();
+        window.__load.counter--;
+        if(window.__load.counter==0)closeLoadingScreen();
     },
 };
 
