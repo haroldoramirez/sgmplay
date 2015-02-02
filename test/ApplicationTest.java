39dbcd49a031aab05b3b80ba5e32fa364f390515
haroldo <haroldoramirez@gmail.com>
2015-02-02 10:38:25 -0200

    removido test do travis.ci

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -6,17 +6,17 @@
 //import com.fasterxml.jackson.databind.JsonNode;
 import org.junit.*;
 
-//import play.mvc.*;
-//import play.test.*;
-//import play.data.DynamicForm;
-//import play.data.validation.ValidationError;
-//import play.data.validation.Constraints.RequiredValidator;
-//import play.i18n.Lang;
-//import play.libs.F;
-//import play.libs.F.*;
-//import play.twirl.api.Content;
+import play.mvc.*;
+import play.test.*;
+import play.data.DynamicForm;
+import play.data.validation.ValidationError;
+import play.data.validation.Constraints.RequiredValidator;
+import play.i18n.Lang;
+import play.libs.F;
+import play.libs.F.*;
+import play.twirl.api.Content;
 
-//import static play.test.Helpers.*;
+import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
 
